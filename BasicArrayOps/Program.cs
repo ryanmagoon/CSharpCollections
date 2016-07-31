15b4d4769c91f99c80ab09b29f8266b39f3b1299
ryanmagoon <mr.magoon5@gmail.com>
2016-07-31 19:22:57 -0400

    Initial Commit

---
+++ BasicArrayOps/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicArrayOps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string monday = "Monday";
+
+            // type[] - declare array, not simple type
+            string[] daysOfWeek =
+            {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+            };
+
+            //Console.WriteLine("Type in index of day to look up> ");
+            //int day = int.Parse(Console.ReadLine());
+            //Console.WriteLine(daysOfWeek[day]);
+            daysOfWeek[5] = "PartyDay";
+            foreach (string day in daysOfWeek)
+            {
+                Console.WriteLine(day);
+            }
+        }
+    }
+}
