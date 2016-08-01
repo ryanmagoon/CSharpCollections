7514c4f61bfae02290c661f17c37586c79631437
ryanmagoon <mr.magoon5@gmail.com>
2016-08-01 10:04:44 -0400

    add code demo for copying arrays

---
+++ ArrayCopy/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayCopy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] squares = { 1, 4, 9, 16 };
+
+            int[] copy = squares.ToArray();
+
+            //int[] copy = new int[8];
+            //squares.CopyTo(copy, 2);
+
+            // Array.copy is more flexible than copyTo
+            // but the .ToArray() extension method is more readable still,
+            // but less flexible than copyTo or copy
+
+            foreach (int value in copy)
+            {
+                Console.WriteLine(value);
+            }
+            Console.WriteLine(string.Format("squares == copy? {0}", squares == copy));
+        }
+    }
+}
