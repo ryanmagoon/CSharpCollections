bcaca2b9a0fd5834db2f4f43db51dec236be3f1e
ryanmagoon <mr.magoon5@gmail.com>
2016-08-01 10:18:14 -0400

    add code demo for sorting arrays

---
+++ SortArray/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] daysOfWeek = {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+            };
+
+            var comparer = new StringLengthComparer();
+            // Array.Sort() also works in-place, no new array.
+            Array.Sort(daysOfWeek, comparer);
+
+            foreach (string day in daysOfWeek)
+            {
+                Console.WriteLine(day);
+            }
+        }
+    }
+
+    class StringLengthComparer : IComparer<string>
+    {
+        public int Compare(string x, string y)
+        {
+            return x.Length.CompareTo(y.Length);
+        }
+    }
+}
