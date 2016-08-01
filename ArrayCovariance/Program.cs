1b9de340a238ac1b6c7f22a1464b579e835406ab
ryanmagoon <mr.magoon5@gmail.com>
2016-08-01 09:53:47 -0400

    Add array covariance code demo

---
+++ ArrayCovariance/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayCovariance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object[] objArr = new object[3];
+            string[] daysOfWeek =
+            {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+            };
+
+            objArr[0] = 3;
+            Console.WriteLine("objArr[0] = " + objArr[0]);
+
+
+            // completely different types
+            // you can always implicitly case an array of a derived type to a base type
+            // This is known as array covariance
+            object[] objArr2 = daysOfWeek;
+            objArr2[0] = 3;
+            // BOOM! ArrayTypeMismatchException
+            Console.WriteLine("objArr2[0] = " + objArr2[0]);
+
+            //foreach (object obj in objArr2)
+            //{
+            //    Console.WriteLine(obj);
+            //}
+
+            // Array covariance turns compile-time errors into run-time errors
+            // Array covariance is broken, and we shouldn't use it.
+
+            // Covariance for IEnumerable<T> and IEnumerator<T> is not broken. No other collection type
+            // supports covariance.
+        }
+    }
+}
