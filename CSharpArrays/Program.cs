cc66502a3c0b8a4103d509cba64ffa3556d4c75c
ryanmagoon <mr.magoon5@gmail.com>
2016-07-31 23:40:04 -0400

    Add code for array initialization

--- CSharpArrays/Program.cs
+++ CSharpArrays/Program.cs
@@ -10,6 +10,22 @@ namespace CSharpArrays
     {
         static void Main(string[] args)
         {
+            // initialized as 0 (int/value type)
+            int x1;
+
+            // initialized as null (reference type)
+            int[] x2;
+
+            int x3 = 5;
+
+            int[] x4 = new int[5];
+
+            // Array elements are always implicitly initialized, but directly declared variables are not.
+            Console.WriteLine(x4[0]);
+
+            // int[] x5 = new int[5] { 1, 4, 9, 16, 25 };
+            // You can omit any redundant information and still successfully instantiate the array
+            int[] x5 = { 1, 4, 9, 16, 25 };
         }
     }
 }
