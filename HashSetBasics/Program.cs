b7f7f7601036458d1cbfd60cafb035729acdd5ec
ryanmagoon <mr.magoon5@gmail.com>
2016-08-01 18:21:50 -0400

    add basic HashSet functionality

---
+++ HashSetBasics/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashSetBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bigCities = new HashSet<string>
+            {
+                "New York",
+                "Manchester",
+                "Sheffield",
+                "Paris"
+            };
+
+            bool addedSheffield = bigCities.Add("Sheffield");
+            bool addedBeijing = bigCities.Add("Beijing");
+            Console.WriteLine($"Added Sheffield? {addedSheffield}");
+            Console.WriteLine($"Added Beijing? {addedBeijing}");
+            Console.WriteLine();
+
+
+            foreach (string city in bigCities)
+            {
+                Console.WriteLine(city);
+            }
+        }
+    }
+}
