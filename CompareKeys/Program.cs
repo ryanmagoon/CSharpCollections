ab23639a6be4e5741869d30e929eb5808bad8461
ryanmagoon <mr.magoon5@gmail.com>
2016-08-01 14:49:10 -0400

    Write a custom equality comparer

---
+++ CompareKeys/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompareKeys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var primeMinisters = new Dictionary<string, PrimeMinister>(new UncasedStringEqualityComparer())
+            {
+                {"JC", new PrimeMinister("James Callaghan", 1976) },
+                {"MT", new PrimeMinister("Margaret Thatcher", 1979) },
+                {"TB", new PrimeMinister("Tony Blair", 1997) }
+            };
+
+            Console.WriteLine(primeMinisters["tb"]);
+        }
+    }
+
+    class UncasedStringEqualityComparer : IEqualityComparer<string>
+    {
+        public bool Equals(string x, string y)
+        {
+            return x.ToUpper() == y.ToUpper();
+        }
+
+        public int GetHashCode(string obj)
+        {
+            return obj.ToUpper().GetHashCode();
+        }
+    }
+}
