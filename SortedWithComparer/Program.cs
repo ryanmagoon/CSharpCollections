8b6c0e4fbba29fd3ecb6ff6fdc6d1592016e8852
ryanmagoon <mr.magoon5@gmail.com>
2016-08-01 15:09:23 -0400

    Implement a custom IComparer<T>

---
+++ SortedWithComparer/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortedWithComparer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var primeMinisters = new SortedList<string, PrimeMinister>
+                (new UncasedStringComparer())
+            {
+                {"JC", new PrimeMinister("James Callaghan", 1976) },
+                {"MT", new PrimeMinister("Margaret Thatcher", 1979) },
+                {"TB", new PrimeMinister("Tony Blair", 1997) }
+            };
+
+            primeMinisters.Add("JM", new PrimeMinister("John Major", 1990));
+
+            Console.WriteLine($"tony is {primeMinisters["tb"]}");
+        }
+    }
+
+    class UncasedStringComparer : IComparer<string>
+    {
+        public int Compare(string x, string y)
+        {
+            return string.Compare(x, y, StringComparison.InvariantCultureIgnoreCase);
+        }
+    }
+}
