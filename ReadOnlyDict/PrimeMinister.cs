c441b587e0b7ccca7bf3ade0ca97b028a05fd8f2
ryanmagoon <mr.magoon5@gmail.com>
2016-08-01 14:55:29 -0400

    Added demo for ReadOnlyDictionary<TKey, TValue>

---
+++ ReadOnlyDict/PrimeMinister.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadOnlyDict
+{
+    class PrimeMinister
+    {
+        public string Name { get; private set; }
+
+        public int YearElected { get; private set; }
+
+        public PrimeMinister(string name, int yearElected)
+        {
+            this.Name = name;
+            this.YearElected = yearElected;
+        }
+
+        public override string ToString()
+        {
+            return string.Format($"{Name}, elected: {YearElected}");
+        }
+    }
+}
