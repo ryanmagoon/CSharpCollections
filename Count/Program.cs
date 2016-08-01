a3220c1658f18b84bbc2b93c9095923bccc1012d
ryanmagoon <mr.magoon5@gmail.com>
2016-08-01 11:57:40 -0400

    Add example code for ICollection<T> IsReadOnly property

--- Count/Program.cs
+++ Count/Program.cs
@@ -21,10 +21,24 @@ namespace Count
                 "Sunday"
             };
             ICollection<string> collection = (ICollection<string>)daysOfWeek;
-            Console.WriteLine("Count() = " + collection.Count());
-            // returns a cached variable, much faster than the extension method
-            Console.WriteLine("Count = " + collection.Count);
-            Console.WriteLine("Length = " + daysOfWeek.Length);
+
+            // Can still replace elements even though ICollection<T>.IsReadOnly
+            // Only blocks use of ICollection<T> methods, such as Add
+            (collection as string[])[5] = "SlaveDay";
+            if (!collection.IsReadOnly)
+                collection.Add("SlaveDay");
+            else
+                Console.WriteLine("Collection is read-only");
+
+            foreach (string day in collection)
+            {
+                Console.WriteLine(day);
+            }
+
+            //Console.WriteLine("Count() = " + collection.Count());
+            //// returns a cached variable, much faster than the extension method
+            //Console.WriteLine("Count = " + collection.Count);
+            //Console.WriteLine("Length = " + daysOfWeek.Length);
         }
     }
 }
