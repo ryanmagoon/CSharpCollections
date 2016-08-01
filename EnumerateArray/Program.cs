001211dd5640b5b41c5eab557fe31d853797ce50
ryanmagoon <mr.magoon5@gmail.com>
2016-07-31 23:51:30 -0400

    add array enumeration

---
+++ EnumerateArray/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumerateArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] daysOfWeek = {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+            };
+
+            for (int i = 0; i < daysOfWeek.Length; i++)
+            {
+                Console.WriteLine(daysOfWeek[i]);
+            }
+
+            foreach (string day in daysOfWeek)
+            {
+                Console.WriteLine(day);
+            }
+        }
+    }
+}
