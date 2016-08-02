bc3253bcb210df2f773fffe06b461ecf8f922550
ryanmagoon <mr.magoon5@gmail.com>
2016-08-01 23:28:09 -0400

    add code demo for enumerator covariance

---
+++ Covariance/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Covariance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Hello, World!";
+            object obj = str;
+
+            var strList = new List<string>
+            {
+                "Monday",
+                "Tuesday"
+            };
+
+            IEnumerable<object> objEnum = strList;
+        }
+    }
+}
