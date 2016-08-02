20feb405a53677c8cbfa48b06d6477b9b850b8e8
ryanmagoon <mr.magoon5@gmail.com>
2016-08-01 22:14:10 -0400

    add set comparisons demo

---
+++ SetComparisons/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SetComparisons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bigCities = new HashSet<string>
+            {
+                "New York",
+                "Manchester",
+                "Sheffield",
+                "Paris"
+            };
+
+            var ukCities = new HashSet<string>
+            {
+                "Sheffield",
+                "Ripon",
+                "Truro",
+                "Manchester"
+            };
+
+            var bigCitiesArr = new HashSet<string>
+            {
+                "New York",
+                "Manchester",
+                "Sheffield",
+                "Paris"
+            };
+
+            // bool ukIsSubset = ukCities.IsSubsetOf(bigCitiesArr);
+            bool ukIsSubset = bigCitiesArr.IsSupersetOf(ukCities);
+            Console.WriteLine($"UK cities subset of big cities?");
+        }
+    }
+}
---
+++ SetEquals/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SetEquals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bigCities = new HashSet<string>
+            {
+                "New York",
+                "Manchester",
+                "Sheffield",
+                "Paris"
+            };
+
+            var citiesInUk = new HashSet<string>
+            {
+                "Sheffield",
+                "Ripon",
+                "Truro",
+                "Manchester"
+            };
+
+            var bigCitiesArr = new string[]
+            {
+                "New York",
+                "Manchester",
+                "Sheffield",
+                "Paris"
+            };
+
+            bool areEqual = bigCities.SetEquals(bigCitiesArr);
+            Console.WriteLine($"bigCities = bigCitiesArr? {areEqual}");
+
+            bool areEqualUk = citiesInUk.SetEquals(bigCitiesArr);
+            Console.WriteLine($"citiesInUk = bigCitiesArr? {areEqualUk}");
+        }
+    }
+}
