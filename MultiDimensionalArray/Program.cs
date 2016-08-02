fa8d4d5a2c3dfddf9e0c58ee52e48de4cf864fd6
ryanmagoon <mr.magoon5@gmail.com>
2016-08-01 23:50:59 -0400

    add code demo for getlowerbound and getupperbound

--- MultiDimensionalArray/Program.cs
+++ MultiDimensionalArray/Program.cs
@@ -12,9 +12,9 @@ namespace MultiDimensionalArray
         {
             float[,] tempsGrid = new float[4, 3];
 
-            for (int x = 0; x < tempsGrid.GetLength(0); x++)
+            for (int x = tempsGrid.GetLowerBound(0); x <= tempsGrid.GetUpperBound(0); x++)
             {
-                for (int y = 0; y < tempsGrid.GetLength(1); y++)
+                for (int y = tempsGrid.GetLowerBound(1); y <= tempsGrid.GetUpperBound(1); y++)
                 {
                     tempsGrid[x, y] = x + 10 * y;
                 }
