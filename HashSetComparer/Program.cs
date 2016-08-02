6af6bdcba3709bf18e336770e4714c9dd6e69078
ryanmagoon <mr.magoon5@gmail.com>
2016-08-01 21:04:04 -0400

    Add demo code for set intersections

---
+++ HashSetComparer/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashSetComparer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bigCities = new HashSet<string>(new UncasedStringEqualityComparer())
+            {
+                "New York",
+                "Manchester",
+                "Sheffield",
+                "Paris"
+            };
+
+            bigCities.Add("SHEFFIELD");
+            bigCities.Add("BEIJING");
+
+            foreach (string city in bigCities)
+            {
+                Console.WriteLine(city);
+            }
+        }
+    }
+
+    class UncasedStringEqualityComparer : IEqualityComparer<string>
+    {
+        public bool Equals(string x, string y)
+        {
+            return x.ToUpper() == y.ToUpper();
+        }
+
+        public int GetHashCode(string obj)
+        {
+            return obj.ToUpper().GetHashCode();
+        }
+    }
+}
---
+++ Intersect/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intersect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bigCities = new HashSet<string>
+            {
+                "New York",
+                "Manchester",
+                "Sheffield",
+                "Paris"
+            };
+
+            string[] citiesInUk =
+            {
+                "Sheffield",
+                "Ripon",
+                "Truro",
+                "Manchester"
+            };
+
+            // The LINQ Intersect method doesn't change the original HashSet
+            // It returns a new enumerable
+            var newCities = bigCities.Intersect(citiesInUk);
+            foreach (string city in newCities)
+            {
+                Console.WriteLine(city);
+            }
+
+            //bigCities.IntersectWith(citiesInUk);
+            //foreach (string city in bigCities)
+            //{
+            //    Console.WriteLine(city);
+            //}
+        }
+    }
+}
