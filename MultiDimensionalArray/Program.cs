1f7674acdf7e53e6486f8df3a9bf2948d34086d4
ryanmagoon <mr.magoon5@gmail.com>
2016-08-01 23:41:18 -0400

    add multidimensional array demo

---
+++ MultiDimensionalArray/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiDimensionalArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float[,] tempsGrid = new float[4, 3];
+
+            for (int x = 0; x < 4; x++)
+            {
+                for (int y = 0; y < 3; y++)
+                {
+                    tempsGrid[x, y] = x + 10 * y;
+                }
+            }
+
+            foreach (float temperature in tempsGrid)
+            {
+                Console.WriteLine(temperature);
+            }
+
+            for (int x = 0; x < 4; x++)
+            {
+                for (int y = 0; y < 3; y++)
+                {
+                    Console.WriteLine($"{tempsGrid[x,y]}, ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
