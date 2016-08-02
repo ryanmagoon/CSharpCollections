13bcc267fd3abb312e22bf640bdfd0eb41452980
ryanmagoon <mr.magoon5@gmail.com>
2016-08-01 23:46:32 -0400

    add demo code for getting length and rank

--- MultiDimensionalArray/Program.cs
+++ MultiDimensionalArray/Program.cs
@@ -12,24 +12,22 @@ namespace MultiDimensionalArray
         {
             float[,] tempsGrid = new float[4, 3];
 
-            for (int x = 0; x < 4; x++)
+            for (int x = 0; x < tempsGrid.GetLength(0); x++)
             {
-                for (int y = 0; y < 3; y++)
+                for (int y = 0; y < tempsGrid.GetLength(1); y++)
                 {
                     tempsGrid[x, y] = x + 10 * y;
                 }
             }
 
-            foreach (float temperature in tempsGrid)
-            {
-                Console.WriteLine(temperature);
-            }
+            Console.WriteLine($"Length is {tempsGrid.Length}");
+            Console.WriteLine($"Rank is {tempsGrid.Rank}");
 
-            for (int x = 0; x < 4; x++)
+            for (int x = 0; x < tempsGrid.GetLength(0); x++)
             {
-                for (int y = 0; y < 3; y++)
+                for (int y = 0; y < tempsGrid.GetLength(1); y++)
                 {
-                    Console.WriteLine($"{tempsGrid[x,y]}, ");
+                    Console.Write($"{tempsGrid[x,y]}, ");
                 }
                 Console.WriteLine();
             }
