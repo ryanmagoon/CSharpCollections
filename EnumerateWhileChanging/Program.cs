182cc6809f96950937ac16750dc387a40485aac3
ryanmagoon <mr.magoon5@gmail.com>
2016-08-01 23:07:55 -0400

    add code demo for modification while enumerating

---
+++ EnumerateWhileChanging/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumerateWhileChanging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var days = new List<string>
+            //{
+            //    "Monday",
+            //    "Tuesday",
+            //    "Wednesday"
+            //};
+
+            var days = new string[]
+            {
+                "Monday",
+                "Tuesday",
+                "Wednesday"
+            };
+            foreach (string day in days)
+            {
+                days[1] = "2nd day";
+                Console.WriteLine(day);
+            }
+        }
+    }
+}
