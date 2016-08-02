6bfbca022dd76263cc529c4ba5df4356f0ea844d
ryanmagoon <mr.magoon5@gmail.com>
2016-08-01 23:18:57 -0400

    Implement custom enumerator

---
+++ ImplementEnumerator/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImplementEnumerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            AllDaysOfWeek allDays = new AllDaysOfWeek();
+
+            foreach (string day in allDays)
+            {
+                Console.WriteLine(day);
+            }
+        }
+    }
+
+    public class AllDaysOfWeek : IEnumerable<string>
+    {
+        public IEnumerator<string> GetEnumerator()
+        {
+            Console.WriteLine("Calling generic enumerator");
+            yield return "Monday";
+            yield return "Tuesday";
+            yield return "Wednesday";
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
